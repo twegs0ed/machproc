dc9564904e6fbb84f0add5fec1cc25bd5b5ff25b
Иванов Владимир Владимирович <ivanov_vv@cpt-pskov.ru>
2024-01-29 10:36:25 +0300

    '1'

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6sx37h-oypvd1w--d^x476dv^*6ln$hf7t1k&2x%l*t^1w#xwd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
