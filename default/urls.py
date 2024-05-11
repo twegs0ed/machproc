0b0dec985eb54c4bfab4ac6f5f822d7ff5059dc7
twegs0ed <tweg@bk.ru>
2024-05-11 16:22:39 +0300

    '1'

--- default/urls.py
+++ default/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, GalleryView, AboutusView
  
 urlpatterns = [
-    path('', IndexView, name='index')
+    path('', IndexView, name='index'),
+    path('gallery', GalleryView, name='Gallery'),
+    path('about-us', AboutusView, name='Aboutus')
 ]
--- default/views.py
+++ default/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def IndexView(request):
     return render(request, "index.html", {'title': 'Технологии механической обработки'})
+
+def GalleryView(request):
+    return render(request, "gallery.html", {'title': 'Галерея'})
+def AboutusView(request):
+    return render(request, "aboutus.html", {'title': 'О компании ООО "ТехМех"'})
